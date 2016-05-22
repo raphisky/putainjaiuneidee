33a13ba88ae6f5998aabed1224c30df3f8d08376
François Vieille <vieille.francois@gmail.com>
2016-05-22 02:03:04 +0200

    Ajout du random/spreadsheet

---
+++ pjui.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+	noIdeas = function(){
+		return ["No ideas."]
+	}
+
+	getIdeasFromFile = function(gss){
+		var ideas = [];
+		for(var el in gss.feed.entry){
+			ideas.push(gss.feed.entry[el].title.$t);
+		}
+		return ideas;
+	}
+
+	getRandomIdea = function(ideas){
+		if(ideas.length > 0){
+			var i = Math.floor(Math.random()*ideas.length);
+			var r = ideas[i];
+			ideas.splice(i, 1);
+		} else {
+			var r = getRandomIdea(noIdeas());
+		}
+		return r;
+	}
+
+	printRandomIdea = function(ideas){
+		$('#idee_display').text(getRandomIdea(ideas));
+	}
+
+	$.getJSON('https://spreadsheets.google.com/feeds/list/1954RrPSlDyFIyIsyjObpqDgKlPWBUaW2SUaV72j5oO0/2/public/basic?alt=json')
+	 .always(function(ideas, textStatus){
+	 	if(textStatus == "error")
+	 		var ideas = noIdeas()
+	 	else
+	 		var ideas = getIdeasFromFile(ideas);
+
+	 	console.log(ideas)
+
+	 	//on load
+	 	printRandomIdea(ideas)
+	 	//au click
+	 	$('.moar').on('click', function(){
+		 	printRandomIdea(ideas);
+	 	})
+	 })
+
+})
