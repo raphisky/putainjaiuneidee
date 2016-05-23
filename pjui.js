97812fdd6a89893657fff029f60ac6e53bfa0f00
François Vieille <vieille.francois@gmail.com>
2016-05-24 00:01:56 +0200

    Ajout sollicitation

--- pjui.js
+++ pjui.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
 
 	var ideas = [];
+	var sollications = ["Ajoute la tienne si tu en as une ! <br> <a class='typeform-share link' href='https://raphisky.typeform.com/to/ITMPGZ' data-mode='2' target='_blank'>Ajouter une putain d'idée</a>",
+						"Ajoute la tienne si tu en as une ! <br> <a class='typeform-share link' href='https://raphisky.typeform.com/to/ITMPGZ' data-mode='2' target='_blank'>Ajouter une putain d'idée</a>"];
+	var click = 0;
+	var isSollicated = false;
 
 	noIdeas = function(){
 		return ["Plus d'idée. Ajoute la tienne si tu en as une ! <br> <a class='typeform-share link' href='https://raphisky.typeform.com/to/ITMPGZ' data-mode='2' target='_blank'>Ajouter une putain d'idée</a>"];
@@ -22,21 +26,32 @@ $(document).ready(function(){
 			ideas.splice(i, 1);
 		} else {
 			var r = getRandomIdea(noIdeas());
-			html = true;
+			html  = true;
 		}
 		return [r, html];
 	}
 
-	printRandomIdea = function(ideas){
-		var ideaArray = getRandomIdea(ideas);
-		var idea      = ideaArray[0];
-		var html      = ideaArray[1];
+	printRandomIdea = function(ideas, click){
+		var idea, html;
 		var $display  = $('#idee_display');
+		var proba = 1/7;
+		if(Math.random() < proba && click > 5 && isSollicated == false){
+			idea = sollications[Math.floor(Math.random()*sollications.length)];
+			isSollicated = true;
+			html = true;
+		} else {
+			var ideaArray = getRandomIdea(ideas);
+			idea          = ideaArray[0];
+			html          = ideaArray[1];
+			isSollicated  = false;
+		}
+
 		if(html)
 			$display.html(idea);
 		else
 			$display.text(idea);
 	}
+
 
 	$.getJSON('https://spreadsheets.google.com/feeds/list/1954RrPSlDyFIyIsyjObpqDgKlPWBUaW2SUaV72j5oO0/2/public/basic?alt=json')
 	 .done(function(ideas){
@@ -44,16 +59,18 @@ $(document).ready(function(){
 	 	console.log(ideas);
 
 	 	//on load
-	 	printRandomIdea(ideas)
+	 	printRandomIdea(ideas, click)
 	 	//au click
 	 	$('.moar').on('click', function(){
 	 		console.log(ideas.length);
-		 	printRandomIdea(ideas);
+	 		click++;
+		 	printRandomIdea(ideas, click);
+		 	console.log(click);
 	 	})
 	 })
 	 .fail(function(){
 	 	// en cas de fail
-	 	printRandomIdea(ideas)
+	 	printRandomIdea(ideas, click)
 	 })
 
 })
