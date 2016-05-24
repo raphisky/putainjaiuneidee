0198e7cae468bacdf9b82f4de5155c6efcb130a6
François Vieille <vieille.francois@gmail.com>
2016-05-25 01:56:56 +0200

    Fuck random ;)

--- pjui.js
+++ pjui.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
 						"Plus on est de fous, plus on rit : <br> <a class='typeform-share link' href='https://raphisky.typeform.com/to/ITMPGZ' data-mode='2' target='_blank'>Ajouter une putain d'idée</a>","Toi aussi, participe <br> <a class='typeform-share link' href='https://raphisky.typeform.com/to/ITMPGZ' data-mode='2' target='_blank'>Oh que oui</a>"];
 	var click = 0;
 	var isSollicated = false;
+	var ordre = "random";
+	var lastKey = null;
 
 	noIdeas = function(){
 		return ["Plus d'idée. Ajoute la tienne si tu en as une ! <br> <a class='typeform-share link' href='https://jaiuneidee.typeform.com/to/ITMPGZ' data-mode='2' target='_blank'>Ajouter une putain d'idée</a>"];
@@ -20,8 +22,22 @@ $(document).ready(function(){
 
 	getRandomIdea = function(ideas){
 		var html = false;
+		console.log(ordre);
 		if(ideas.length > 0){
-			var i = Math.floor(Math.random()*ideas.length);
+			if(ordre == "random")
+				var i = Math.floor(Math.random()*ideas.length);
+			else if(ordre == "date_desc")
+				var i = ideas.length-1;
+			else if(ordre == "date_asc")
+				var i = 0;
+			else if(ordre == "length_asc"){
+				tmp = ideas.map(function(el){return el.length;})
+				var i = tmp.indexOf(Math.min.apply(Math, tmp))
+			}
+			else if(ordre == "length_desc"){
+				tmp = ideas.map(function(el){return el.length;})
+				var i = tmp.indexOf(Math.max.apply(Math, tmp))
+			}
 			var r = ideas[i];
 			ideas.splice(i, 1);
 		} else {
@@ -71,6 +87,21 @@ $(document).ready(function(){
 	 .fail(function(){
 	 	// en cas de fail
 	 	printRandomIdea(ideas, click)
+	 })
+
+	 $(document).keyup(function(e){
+	 	var code = e.keyCode || e.which;
+		if(code == 68) {
+			ordre = (ordre == "date_desc")?"date_asc":"date_desc";
+			lastKey = 68;
+		} else if (code == 82){
+			ordre = "random";
+		} else if (code == 83 && e.shiftKey){
+			ordre = "length_asc";
+		} else if (code == 83){
+			ordre = "length_desc";
+		}
+		console.log(ordre);
 	 })
 
 })
